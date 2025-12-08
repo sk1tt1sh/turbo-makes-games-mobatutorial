832f8d2b627d0a3215215ad6a2c843d40a9618e2
Troy Harshberger <troy.harshberger@gmail.com>
2025-12-07 17:15:45 -0700

    Update other bridge and rename for clarity. Clean up comments and debug. Gravity isn't right though

--- Assets/_Scripts/Common/ChampMoveSystem.cs
+++ Assets/_Scripts/Common/ChampMoveSystem.cs
@@ -25,23 +25,17 @@ public partial struct ChampMoveSystem : ISystem {
         .WithAll<Simulate>()
         .WithEntityAccess()) {
 
-      //Gets the value at the current network tick as this could potentially change
       float3 moveTarget;
 
-
       //Check if the champ has an autoattack target and override the move position
-      
       if(SystemAPI.HasComponent<ChampTargetEntity>(entity)) {
         var target = SystemAPI.GetComponentRW<ChampTargetEntity>(entity);
         var hasLocalTransform = SystemAPI.HasComponent<LocalTransform>(target.ValueRO.Target);
 
-        //Debug.Log($"ChampTargetEntity exists. Target Entity: {target.ValueRO.Target.Index}, " +
-        //  $"Has Transform:{hasLocalTransform.ToString()} on {(state.WorldUnmanaged.IsServer()?"server":"client")}");
-
         if(hasLocalTransform) {
           var targetPos = SystemAPI.GetComponent<LocalTransform>(target.ValueRO.Target);
           moveTarget = targetPos.Position;
-          //moveTarget.y = transform.ValueRO.Position.y;
+          moveTarget.y = transform.ValueRO.Position.y;
 
           //TODO: Add a component to champ for their auto attack range
           if(math.distance(transform.ValueRO.Position, moveTarget) < 9f) {
@@ -51,35 +45,24 @@ public partial struct ChampMoveSystem : ISystem {
         }
         else {
           moveTarget = movePosition.ValueRO.Value;
-          // This prevents the player from moving into the ground
           moveTarget.y = transform.ValueRO.Position.y;
-          //if(state.WorldUnmanaged.IsServer())
-            //Debug.LogWarning("Target entity has no transform (server)");
           //Proably need to remove that target entity. The champion will probably move to last target position
           //We might be able to use a hack on the component data and set the position as the target moves
         }
       }
       else {
         moveTarget = movePosition.ValueRO.Value;
-        // This prevents the player from moving into the ground
-        //moveTarget.y = transform.ValueRO.Position.y;
+        moveTarget.y = transform.ValueRO.Position.y;
 
         if(math.distance(transform.ValueRO.Position, moveTarget) < 1f) {
-          //If the move distance (Likely an absolute value) is too small don't do anything. 
-          //Debug.Log("Move target reached");
           continue;
         }
       }
 
-      //Debug.Log($"Moving. The moveTarget value {(moveTarget.Equals(movePosition.ValueRO.Value)?"is equal":"is not equals")} " +
-      //  $"to the movePosition input value");
       float3 moveDirection = math.normalize(moveTarget - transform.ValueRO.Position);
       var moveVector = moveDirection * moveSpeed.ValueRO.Value * deltaTime;
-      //Debug.Log($"Move Vector {moveVector}");
       transform.ValueRW.Position += moveVector;
-      //This essentially points the character in the direction that it's going
       transform.ValueRW.Rotation = quaternion.LookRotationSafe(moveDirection, math.up());
-      //Debug.Log($"About to move toward: {moveTarget}. Vector: {moveVector}");
     }
   }
 }
